8016fcc3ed59b2e857f19e9468939db57d494737
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-09-08 22:07:54 +0530

    Completed 3d spiral plot

--- 3D Spiral Plot/app.py
+++ 3D Spiral Plot/app.py
@@ -7,4 +7,10 @@ xline = np.sin(zline)
 yline = np.cos(zline)
 zline = np.linspace(0, 15, 1000)
 
-ax.plot3D(xline, yline, zline, 'white')
+ax.plot3D(xline, yline, zline, 'black')
+
+xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+zdata = 15 * np.random.random(100)
+
+print(ax.scatter3D(xdata, ydata, zdata, c=zdata, s=30, cmap='Greens'))
