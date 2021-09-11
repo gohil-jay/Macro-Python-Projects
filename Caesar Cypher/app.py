c5713bde440b6928beee7383d9f2d243f9a3bea9
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-09-11 10:47:17 +0530

    Added decryption

--- Caesar Cypher/app.py
+++ Caesar Cypher/app.py
@@ -9,11 +9,24 @@ def encrypt(message, num):
  
     return result
 
+def decrypt(message, num):
+    result = ""
+    for i in range( len(message) ):
+        char = message[i]
+        if (char.isupper()):
+            result += chr((ord(char) - num-65) % 26 + 65) 
+        else:
+            result += chr((ord(char) - num-97) % 26 + 97)
+ 
+    return result
+
 message = input("Enter the message : ")
 print("\n")
 num = int(input("Enter ciper shift : "))
 print("\n")
 
-en = encrypt(message, num)
+message1 = encrypt(message, num)
+message2 = decrypt(message1, num)
 
-print("The encrypted message is : ", en)
+print("The encrypted message is : ", message1)
+print("The decrypted message is : ", message2)
