7e6541a2d299053bd17780b1c5acc47696e08a0c
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-09-17 11:31:46 +0530

    Completed the macro project

--- Sound from Image/app.py
+++ Sound from Image/app.py
@@ -2,15 +2,22 @@ from PIL import Image
 from gtts import gTTS
 from pytesseract import image_to_string
 
-def image_to_sound(path_to_image):
-        
-    loaded_image = Image.open(path_to_image)
+def image_to_sound(image_path):
+    try:
+        loaded_image = Image.open(image_path)
         decoded_text = image_to_string(loaded_image)
         cleaned_text = " ".join(decoded_text.split("\n"))
         
         print(cleaned_text)
         
         sound = gTTS(cleaned_text, lang="en")
-        sound.save("test_sound.mp3")
+        sound.save("image_to_sound.mp3")
+        return True
+    
+    except Exception as bug:
+        print("The bug thrown : ", bug)
+        return False
 
-image_to_sound("test_image.jpeg")
+if __name__ == "__main__":
+    image_to_sound("text_image.jpg")
+    input()
