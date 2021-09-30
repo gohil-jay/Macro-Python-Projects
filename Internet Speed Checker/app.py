67383991795ca5e66a1419ed342c266abc578526
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-09-30 14:14:23 +0530

    Added a new function in module I found for ping

--- Internet Speed Checker/app.py
+++ Internet Speed Checker/app.py
@@ -8,3 +8,7 @@ temp = speedtest.Speedtest()
 
 print("Your download speed is : ", temp.download()//10**6, "MBPS")
 print("Your upload speed is : ", temp.upload()//10**6, "MBPS")
+
+servernames = []
+temp.get_servers(servernames)
+print("Your ping speed is : ", temp.results.ping)
