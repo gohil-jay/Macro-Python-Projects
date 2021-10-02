b4e10d905af40b1adec6b4d087b86098fe05c116
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-10-02 09:28:10 +0530

    Restructured the merged PR file code

--- Strong_password_generator/app.py
+++ Strong_password_generator/app.py
@@ -1,31 +1,32 @@
-#strong password generator of given length
-
-import random
-import string
-import time
+import random, time, string
 
 def passgen(len):
   time.sleep(1.5)  
   print("Generating a strong password...")
+  
   lower = string.ascii_lowercase
   time.sleep(1.5)
   print("Adding lowercase characters...")
+  
   upper = string.ascii_uppercase
   time.sleep(1.5)
   print("Adding uppercase characters...")
+  
   num = string.digits
   time.sleep(1.5)
   print("Adding symbols...")
+  
   symbols = string.punctuation
   time.sleep(1.5)
   print("Shuffling the data to make password strong")
+  
   combined = lower + upper + num + symbols
   temp = random.sample(combined,len)
   pd = "".join(temp)
   return pd
   
-len = int(input("Enter length of password you want : "))    
+len = int(input("Enter length of the password you want : "))
 
 password = passgen(len)
 time.sleep(1)
-print(f"\n\tYour strong password is : {password}")
+print(f"\nYour strong password is : {password}")
