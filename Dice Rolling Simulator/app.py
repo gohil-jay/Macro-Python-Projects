3b5b3e027413a6b7432e79ded1047b3f26f925f6
Jay Gohil <59703162+gohil-jay@users.noreply.github.com>
2021-10-18 09:26:42 +0530

    Cleaned the code

--- Dice Rolling Simulator/app.py
+++ Dice Rolling Simulator/app.py
@@ -1,40 +1,29 @@
+import random
 import tkinter
 from PIL import Image, ImageTk
-import random
 
 root = tkinter.Tk()
-root.geometry('400x400')
+root.geometry('500x500')
 root.title('Roll the Dice')
 
 l0 = tkinter.Label(root, text="")
 l0.pack()
-
-l1 = tkinter.Label(root, text="Dice Rolling Simulator", fg = "red",
-        bg = "white",
-        font = "Helvetica 16 bold italic")
+l1 = tkinter.Label(root, text="Dice Rolling Simulator", fg = "red", bg = "white", font = "Helvetica 16 bold italic")
 l1.pack()
 
-
 dice = ['Dice Rolling Simulator/die1.png', 'Dice Rolling Simulator/die2.png', 'Dice Rolling Simulator/die3.png', 'Dice Rolling Simulator/die4.png', 'Dice Rolling Simulator/die5.png', 'Dice Rolling Simulator/die6.png']
-
 image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 
 label1 = tkinter.Label(root, image=image1)
 label1.image = image1
-
 label1.pack( expand=True)
 
 def rolling_dice():
-    image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))
-    
+    image1 = ImageTk.PhotoImage(Image.open(random.choice(dice)))    
     label1.configure(image=image1)
-   
     label1.image = image1
 
-
 button = tkinter.Button(root, text='Roll the Dice', fg='red', command=rolling_dice)
-
 button.pack( expand=True)
-
 
 root.mainloop()
