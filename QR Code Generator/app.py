749eb1ffeb704e23c34196a4b0e17e844092d3f7
Jay Gohil <jay.gohil.info@gmail.com>
2021-10-25 14:20:55 +0530

    Update app.py

--- QR Code Generator/app.py
+++ QR Code Generator/app.py
@@ -1,6 +1,8 @@
 """
-Modules to install: pyqrcode
+Modules to install:
+
 !pip install pyqrcode
+!pip install pypng
 """
 
 import pyqrcode
@@ -8,5 +10,5 @@ import pyqrcode
 link = 'https://jay-gohil.me/'
 url =  pyqrcode.create(link)
 
-url.svg('QR.svg', scale=10)
+url.svg('QR.svg', scale=20)
 print(url.terminal(quiet_zone=1))
